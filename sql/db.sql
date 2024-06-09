3d8db2b60cc9efebc83e03f41fd2ab9fd5df10ea
eli <elzr.sr@gmail.com>
2024-06-09 18:57:05 +0200

    feat: timestamp in db

--- sql/db.sql
+++ sql/db.sql
@@ -1,13 +1,15 @@
 create table message (
     id integer primary key autoincrement,
     content text not null,
-    ip_user char(15) not null
+    ip_user char(15) not null,
+    created_at text default (datetime('now','localtime'))
 )
 
 create table reply (
     id integer,
     content text not null,
     id_message integer not null,
+    ip_user char(15) not null,
     primary key (id, id_message),
     foreign key (id_message) references message(id)
 )
