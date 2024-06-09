66f53a95f2ee7cc88c2a308c45c7cd61e95c22f2
eli <elzr.sr@gmail.com>
2024-06-09 19:20:27 +0200

    feat: simplified reply table

--- sql/db.sql
+++ sql/db.sql
@@ -7,10 +7,8 @@ create table message (
 );
 
 create table reply (
-    id integer,
+    id_message integer primary key,
     content text not null check (length(content) <= 140),
-    id_message integer not null,
     ip_user char(15) not null,
-    primary key (id, id_message),
     foreign key (id_message) references message(id)
 );
