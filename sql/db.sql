acae754655d9a7cddaa13bc6526dbb250f3e4000
eli <elzr.sr@gmail.com>
2024-06-09 19:13:17 +0200

    feat: constraints not null and length in db

--- sql/db.sql
+++ sql/db.sql
@@ -1,16 +1,16 @@
 create table message (
     id integer primary key autoincrement,
-    content text not null,
+    content text not null check (length(content) <= 140),
     ip_user char(15) not null,
-    read integer default 0 check (read in (0,1)),
-    created_at text default (datetime('now','localtime'))
-)
+    read integer not null default 0 check (read in (0,1)),
+    created_at text not null default (datetime('now','localtime'))
+);
 
 create table reply (
     id integer,
-    content text not null,
+    content text not null check (length(content) <= 140),
     id_message integer not null,
     ip_user char(15) not null,
     primary key (id, id_message),
     foreign key (id_message) references message(id)
-)
+);
