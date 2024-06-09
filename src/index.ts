d58b9f22e7af9d9292533b22f60b7b2f92f5ba5e
eli <elzr.sr@gmail.com>
2024-06-09 18:33:42 +0200

    feat: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 
 const app = express()
 
+// CORS
+const corsOptions = {
+  origin: 'localhost',
+  methods: ['GET', 'POST'],
+  allowHeaders: ['Content-Type']
+}
+
 // Middlewares
 app.use(express.json())
+app.use(cors(corsOptions))
 
 // Routes
 app.use('/api', router)
