7d1c26e975d8327b40390981ca5761910987cc71
eli <elzr.sr@gmail.com>
2024-06-14 14:03:22 +0200

    feat: addMessage to DB and dotenv location OK

--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,18 @@
-import { createClient } from '@libsql/client/.'
+import { ResultSet, createClient } from '@libsql/client'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const client = createClient({
   url: process.env.DATABASE_URL ?? '',
   authToken: process.env.DATABASE_TOKEN ?? ''
 })
+
+export async function addMessage (content: string, ipUser: string): Promise<ResultSet> {
+  const query = 'INSERT INTO message (content, ip_user) VALUES (?, ?)'
+  const result = await client.execute({
+    sql: query,
+    args: [content, ipUser]
+  })
+  return result
+}
