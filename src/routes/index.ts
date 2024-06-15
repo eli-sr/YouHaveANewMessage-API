82d52a4386d11cb6e24b71d05a3d820308e75624
eli <elzr.sr@gmail.com>
2024-06-15 23:42:55 +0200

    feat: message endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,26 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import message from '../../mocks/message.json'
 import { ApiResponse } from '../types'
+import { getMessage, setMessageRead } from '../../db/client'
 
 const router = Router()
 
-router.get('/message', (_req, res) => {
-  const messageMock: ApiResponse = message
-  res.send(messageMock)
+router.get('/message', async (_req, res) => {
+  const response: ApiResponse = {
+    sentMessage: false
+  }
+  const message = await getMessage()
+  if (message === false) {
+    res.status(404).json({ error: 'There is no message to read' })
+    return
+  }
+  const result = await setMessageRead(message.id)
+  if (!result) {
+    res.status(500).json({ error: 'Internal error' })
+    return
+  }
+  response.lastMessage = message.content
+  res.json(response)
 })
 
 export default router
