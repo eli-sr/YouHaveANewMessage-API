8a189c723d0faf7c5b8b7ab8e88a8e3b43c0e64d
eli <elzr.sr@gmail.com>
2024-06-15 21:59:14 +0200

    feat: Message & Reply types

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,17 @@ export interface ApiResponse {
   lastMessage?: string
   reply?: string
 }
+
+export interface Message {
+  id: number
+  content: string
+  ip_user: string
+  read: boolean
+  created_at: Date
+}
+
+export interface Reply {
+  content: string
+  id_message: number
+  ip_user: string
+}
