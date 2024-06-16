b54e4123bb26bfa453f0bf4cdbf2a2b610c9b923
eli <elzr.sr@gmail.com>
2024-06-16 17:19:12 +0200

    refactor: Moved db script

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,7 +1,7 @@
 create table message (
     id integer primary key autoincrement,
     content text not null check (length(content) <= 140),
-    ip_user char(15) not null,
+    ip_user char(45) not null,
     read integer not null default 0 check (read in (0,1)),
     created_at text not null default (datetime('now','localtime'))
 );
@@ -9,6 +9,6 @@ create table message (
 create table reply (
     id_message integer primary key,
     content text not null check (length(content) <= 140),
-    ip_user char(15) not null,
+    ip_user char(45) not null,
     foreign key (id_message) references message(id)
 );
