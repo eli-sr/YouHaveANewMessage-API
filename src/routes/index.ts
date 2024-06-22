be10b37380aef8c345e40d4b3ec8ef137cfc3326
eli <elzr.sr@gmail.com>
2024-06-22 03:16:26 +0200

    refactor: endpoint message throwing errors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,62 +3,63 @@ import { Router } from 'express'
 import { MessageResponse } from '../types'
 import { addMessage, getLastMessagePosted, getLastMessageRead, getMessage, setMessageRead } from '../db/client'
 import { getIp, isCreatedAtWithinLastDay } from '../utils'
+import ErrorAPI from '../../classes/ErrorAPI'
 
 const router = Router()
 
 router.get('/message', async (req, res) => {
-  const response: MessageResponse = {
-    wait: false
-  }
-  const ip = getIp(req.ip)
-  if (ip === null) {
-    res.status(503).json({ error: 'No service' })
-    return
-  }
+  try {
+    const response: MessageResponse = {
+      wait: false
+    }
+    const ip = getIp(req.ip)
+    if (ip === null) {
+      throw new ErrorAPI('No service', 503)
+    }
 
-  const lastMessageRead = await getLastMessageRead(ip)
-  if (lastMessageRead === null) {
     const message = await getMessage()
+
+    const lastMessageRead = await getLastMessageRead(ip)
+    if (lastMessageRead === null) {
+      if (message === null) {
+        throw new ErrorAPI('There is no message to read', 404)
+      }
+      if (!await setMessageRead(message.id, ip)) {
+        throw new ErrorAPI('Internal error')
+      }
+      response.lastMessage = message.content
+      res.json(response)
+      return
+    }
+
+    const lastMessagePosted = await getLastMessagePosted(ip)
+    if (lastMessagePosted === null) {
+      response.lastMessage = lastMessageRead.content
+      res.json(response)
+      return
+    }
+    if (isCreatedAtWithinLastDay(lastMessagePosted.created_at)) {
+      response.wait = true
+      res.json(response)
+      return
+    }
+
     if (message === null) {
-      res.status(404).json({ error: 'There is no message to read' })
-      return
+      throw new ErrorAPI('There is no message to read', 404)
     }
-    const result = await setMessageRead(message.id, ip)
-    if (!result) {
-      res.status(500).json({ error: 'Internal error' })
-      return
+    if (!await setMessageRead(message.id, ip)) {
+      throw new ErrorAPI('Internal error')
     }
+
     response.lastMessage = message.content
     res.json(response)
-    return
-  }
-
-  const lastMessagePosted = await getLastMessagePosted(ip)
-  if (lastMessagePosted === null) {
-    response.lastMessage = lastMessageRead.content
-    res.json(response)
-    return
-  }
-
-  if (isCreatedAtWithinLastDay(lastMessagePosted.created_at)) {
-    response.wait = true
-    res.json(response)
-    return
-  }
-
-  const message = await getMessage()
-  if (message === null) {
-    res.status(404).json({ error: 'There is no message to read' })
-    return
-  }
-
-  const result = await setMessageRead(message.id, ip)
-  if (!result) {
+  } catch (error) {
+    if (error instanceof ErrorAPI) {
+      res.status(error.statusCode).json({ error: error.message })
+      return
+    }
     res.status(500).json({ error: 'Internal error' })
-    return
   }
-  response.lastMessage = message.content
-  res.json(response)
 })
 
 router.post('/message', async (req, res) => {
