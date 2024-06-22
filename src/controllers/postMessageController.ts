a14fae33c4ff3ed8d5fe12659e8925e5a2ccdb1a
eli <elzr.sr@gmail.com>
2024-06-22 19:30:33 +0200

    feat: throwing errors in post message controller

--- src/controllers/postMessageController.ts
+++ src/controllers/postMessageController.ts
@@ -1,26 +1,30 @@
 import { Request, Response } from 'express'
 import { getIp } from '../utils'
 import { addMessage } from '../db/client'
+import ErrorAPI from '../classes/ErrorAPI'
 
 export default async function postMessageController (req: Request, res: Response): Promise<void> {
-  const { message } = req.body
-  if (typeof message !== 'string' || message.trim() === '') {
-    res.status(400).json({ error: 'Message is required' })
-    return
-  }
-  if (message.length > 140) {
-    res.status(400).json({ error: 'Message is too long' })
-    return
-  }
-  const ip = getIp(req.ip)
-  if (ip === null) {
-    res.status(503).json({ error: 'No service' })
-    return
-  }
-  const result = await addMessage(message, ip)
-  if (!result) {
+  try {
+    const { message } = req.body
+    if (typeof message !== 'string' || message.trim() === '') {
+      throw new ErrorAPI('Message is required', 400)
+    }
+    if (message.length > 140) {
+      throw new ErrorAPI('Message is too long', 400)
+    }
+    const ip = getIp(req.ip)
+    if (ip === null) {
+      throw new ErrorAPI('No service', 503)
+    }
+    if (!await addMessage(message, ip)) {
+      throw new ErrorAPI('Internal error')
+    }
+    res.json({ info: 'Message received' })
+  } catch (error) {
+    if (error instanceof ErrorAPI) {
+      res.status(error.statusCode).json({ error: error.message })
+      return
+    }
     res.status(500).json({ error: 'Internal error' })
-    return
   }
-  res.json({ info: 'Message received' })
 }
