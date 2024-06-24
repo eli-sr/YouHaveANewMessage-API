d7532b4abb9f7b5a5332703d351a6d6981ab248b
eli <elzr.sr@gmail.com>
2024-06-24 23:56:50 +0200

    feat: adding flow errors in post message controller

--- src/controllers/postMessageController.ts
+++ src/controllers/postMessageController.ts
@@ -1,20 +1,32 @@
 import { Request, Response } from 'express'
-import { getIp } from '../utils'
-import { addMessage } from '../db/client'
+import { getIp, isCreatedAtWithinLastDay } from '../utils'
+import { addMessage, getLastMessagePostedSinceDate, getLastMessageRead } from '../db/client'
 import ErrorAPI from '../classes/ErrorAPI'
 
 export default async function postMessageController (req: Request, res: Response): Promise<void> {
   try {
+    const ip = getIp(req.ip)
+    if (ip === null) {
+      throw new ErrorAPI('No service', 503)
+    }
+    const lastMessageRead = await getLastMessageRead(ip)
+    if (lastMessageRead === null) {
+      throw new ErrorAPI('You must read a message before posting', 403)
+    }
+    if (lastMessageRead !== null) {
+      const lastMessagePosted = await getLastMessagePostedSinceDate(ip, lastMessageRead.read_at)
+      console.log({ lastMessagePosted })
+      if (lastMessagePosted !== null && isCreatedAtWithinLastDay(lastMessagePosted.created_at)) {
+        throw new ErrorAPI('You must wait 24 hours to post a new message', 403)
+      }
+    }
+
     const { message } = req.body
     if (typeof message !== 'string' || message.trim() === '') {
       throw new ErrorAPI('Message is required', 400)
     }
     if (message.length > 140) {
       throw new ErrorAPI('Message is too long', 400)
-    }
-    const ip = getIp(req.ip)
-    if (ip === null) {
-      throw new ErrorAPI('No service', 503)
     }
     if (!await addMessage(message, ip)) {
       throw new ErrorAPI('Internal error')
